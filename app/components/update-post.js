0855e41618b89bc6f885754b4d9527cdd824345f
Nicole Freed <nicole.freed@gmail.com>
2017-08-09 13:28:33 -0700

    add updatePostForm() and update(post) actions to update-post.js

--- app/components/update-post.js
+++ app/components/update-post.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updatePostForm: false,
+  actions: {
+    updatePostForm() {
+      this.set('updatePostForm', true);
+    },
+    update(post) {
+      var params = {
+        title: this.get('title') ? this.get('title') : "",
+        author: this.get('author') ? this.get('author') : "",
+        image: this.get('image') ? this.get('image') : "",
+        teaser: this.get('teaser') ? this.get('teaser') : "",
+        fulltext: this.get('fulltext') ? this.get('fulltext') : "",
+      };
+      this.set('updatePostForm', false);
+      this.sendAction('update', post, params);
+    }
+  }
 });
