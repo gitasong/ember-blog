f969914d767684df76d15c41c4351102739b52cb
Nicole Freed <nicole.freed@gmail.com>
2017-08-09 13:34:52 -0700

    remove ternary operator from update(post) action of update-post.js source file

--- app/components/update-post.js
+++ app/components/update-post.js
@@ -8,11 +8,11 @@ export default Ember.Component.extend({
     },
     update(post) {
       var params = {
-        title: this.get('title') ? this.get('title') : "",
-        author: this.get('author') ? this.get('author') : "",
-        image: this.get('image') ? this.get('image') : "",
-        teaser: this.get('teaser') ? this.get('teaser') : "",
-        fulltext: this.get('fulltext') ? this.get('fulltext') : "",
+        title: this.get('title'),
+        author: this.get('author'),
+        image: this.get('image'),
+        teaser: this.get('teaser'),
+        fulltext: this.get('fulltext'),
       };
       this.set('updatePostForm', false);
       this.sendAction('update', post, params);
