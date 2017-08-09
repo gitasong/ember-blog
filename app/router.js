8d220c54239a8f3e2a1b3a831966fee524f48fd2
Nicole Freed <nicole.freed@gmail.com>
2017-08-09 15:32:18 -0700

    add contact route

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('post', {path: '/post/:post_id'});
   this.route('about');
+  this.route('contact');
 });
 
 export default Router;
