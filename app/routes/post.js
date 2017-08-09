4402b6aceb14259b8cef44c8e9b2dcafa49d3096
Nicole Freed <nicole.freed@gmail.com>
2017-08-09 14:05:56 -0700

    move update() and delete() actions from post-tile.js to single post.js route handler

--- app/routes/post.js
+++ app/routes/post.js
@@ -4,4 +4,15 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('post', params.post_id);
   }
+
+  actions: {
+    update(post, params) {
+      this.sendAction('update', post, params);
+    },
+    delete(post) {
+      if (confirm('Are you sure you want to delete this post?')) {
+        this.sendAction('destroyPost', post);
+      }
+    }
+  }
 });
