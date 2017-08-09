327095096b8479cc91d53a49a6cab051bc1e9db4
Nicole Freed <nicole.freed@gmail.com>
2017-08-09 11:20:35 -0700

    add postFormShow() and savePost() actions to new-post.js

--- app/components/new-post.js
+++ app/components/new-post.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewPost: false,
+  actions: {
+    postFormShow() {
+      this.set('addNewPost', true);
+    },
+
+    savePost() {
+      var params = {
+        title: this.get('title'),
+        author: this.get('author'),
+        image: this.get('image'),
+        teaser: this.get('teaser'),
+        fulltext: this.get('fulltext'),
+      };
+      this.set('addNewPost', false);
+      this.sendAction('savePost', params);
+    }
+  }
 });
