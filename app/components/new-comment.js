4a845bb37798d5e80a7be5b9ceb3b1ddcfd45ea5
Nicole Freed <nicole.freed@gmail.com>
2017-08-10 09:37:09 -0700

    add commentFormShow() and saveComment() actions to new-comment.js

--- app/components/new-comment.js
+++ app/components/new-comment.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewComment: false,
+  actions: {
+    commentFormShow() {
+      this.set('addNewComment', true);
+    },
+    saveComment() {
+      var params = {
+        title: this.get('title'),
+        body: this.get('body'),
+        username: this.get('username')
+      };
+      this.set('addNewComment', false);
+      this.sendAction('saveReview', params);
+    }
+  }
 });
