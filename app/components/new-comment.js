44b18bb8bcf3637662050a620eff42a5c4e6e0c0
Nicole Freed <nicole.freed@gmail.com>
2017-08-10 14:33:41 -0700

    add post param to saveComment() action on new-comment.js to get app to FINALLY save comments

--- app/components/new-comment.js
+++ app/components/new-comment.js
@@ -10,10 +10,12 @@ export default Ember.Component.extend({
       var params = {
         title: this.get('title'),
         body: this.get('body'),
-        username: this.get('username')
+        username: this.get('username'),
+        post: this.get('post')
       };
       this.set('addNewComment', false);
       this.sendAction('saveComment', params);
+      console.log(params);
     }
   }
 });
