71b38f319f2b2f8666877d85ff42ba98f306a33b
Nicole Freed <nicole.freed@gmail.com>
2017-08-10 09:00:22 -0700

    add properties to comment model + many-to-one relationship to posts

--- app/models/comment.js
+++ app/models/comment.js
@@ -1,5 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  title: DS.attr(),
+  body: DS.attr(),
+  username: DS.attr(),
+  post: DS.belongsTo('post', { async: true })
 });
