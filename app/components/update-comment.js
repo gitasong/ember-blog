64e74402d538939e03231a7368086d8d6250a7a0
Nicole Freed <nicole.freed@gmail.com>
2017-08-10 19:28:37 -0700

    add update(comment) and updateCommentForm() actions to update-comment.js

--- app/components/update-comment.js
+++ app/components/update-comment.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateCommentForm: false,
+  actions: {
+    updateCommentForm() {
+      this.set('updateCommentForm', true);
+    },
+    update(comment) {
+      var params = {
+        title: this.get('title'),
+        body: this.get('body'),
+        username: this.get('username'),
+      };
+      this.set('updateCommentForm', false);
+      this.sendAction('update', comment, params);
+    }
+  }
 });
