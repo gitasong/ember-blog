6e5a89883cbd8603496143b8b296a3a2053aa47e
Nicole Freed <nicole.freed@gmail.com>
2017-08-10 19:33:59 -0700

    pass update() action to comment-tile.js

--- app/components/comment-tile.js
+++ app/components/comment-tile.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   actions: {
+    update(comment, params) {
+      this.sendAction('update', comment, params);
+    },
     delete(comment) {
       if (confirm('Are you sure you want to delete this comment?')) {
         this.sendAction('destroyComment', comment);
